5c15a47a5ca5832448f5db3d374e2ca49cd51380
JKC90 <jkcostello90@gmail.com>
2019-06-09 14:59:54 -0400

    python scraper pt1

--- recipe_scraper/hf_scraper.py
+++ recipe_scraper/hf_scraper.py
@@ -27,22 +27,63 @@ def loop_through_files():
                 parsed_html = get_parsed_html(raw_html)
                 return parsed_html
 
-def get_div_names(html):
-    divs = html.find_all(['h1','div','span'], {'data-test-id': re.compile(r'recipeDetail*')})
-    divs += html.find_all(['h1','div','span'], {'data-translation-id': re.compile(r'recipe-detail*')})
-    return divs
+def get_ingredients(html):
+    ingredient_payload = []
+    ingredients = html.find('div', {"class": "fela-71c2kl"}).find("div")
+    for i in ingredients:
+        img = i.find("img")
+        name = img["alt"]
+        src = img["src"]
+        amount = i.find("div", {"class": "fela-1qz307e"}).find("p").text
+        ingredient_payload += [{"name": name, "src": src, "amount": amount}]
+    return ingredient_payload
 
+def get_ingredients_not_included(html):
+    ingredient_payload = []
+    ingredients = html.find('div', {"class": "fela-18bp2md"}).find("div")
+    for i in ingredients:
+        img = i.find("img")
+        name = img["alt"]
+        src = img["src"]
+        amount = i.find("div", {"class": "fela-1qz307e"}).find("p").text
+        ingredient_payload += [{"name": name, "src": src, "amount": amount}]
+    return ingredient_payload
+
+
+
+def get_recipe_name(html):
+    name = html.find('h1', {'data-test-id': 'recipeDetailFragment.recipe-name'}).text
+    return name
+
+def get_prep_time(html):
+    prep_time = html.find('div', {'class': 'fela-19qpnoj'}).find_all('span')
+    prep_time = prep_time[2].text
+    return prep_time
+
+def get_nutrition(html):
+    nutrition_payload = []
+    nutrition_tags = html.find('div', {'data-test-id':'recipeDetailFragment.nutrition-values'})
+    calories = nutrition_tags.find('span', text=re.compile(r'. kcal')).text
+    carbs = nutrition_tags.find("span", text="Carbohydrate").parent.text.replace("Carbohydrate", "")
+    fat = nutrition_tags.find("span", text="Fat").parent.text.replace("Fat", "")
+    protein = nutrition_tags.find("span", text="Protein").parent.text.replace("Protein", "")
+    nutrition_payload += [{"calories": calories, "carbs": carbs, "fat": fat, "protein": protein}]
+    return nutrition_payload
 
 
 
 def start_crawling():
-    parsed_html = loop_through_files()
-    return get_div_names(parsed_html)
+    html = loop_through_files()
+    ingredients = get_ingredients(html)
+    ingredients_not_included = get_ingredients_not_included(html)
+    name = get_recipe_name(html)
+    prep_time = get_prep_time(html)
+    nutrition = get_nutrition(html)
+    return {"name": name, "prep_time": prep_time, "ingredients": ingredients, "ingredients_not_included": ingredients_not_included, "nutrition": nutrition}
+
+
+
+
 
 x = start_crawling()
-for r in x:
-    if r.get('data-test-id'):
-        print(r.get('data-test-id'))
-    elif r.get('data-translation-id'):
-        print(r.get('data-translation-id'))
-
+print(x)
