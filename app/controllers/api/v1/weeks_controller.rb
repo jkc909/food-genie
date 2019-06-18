d5ffbea6ee293f9c4b1cc9467c59693ebff1b9d0
JKC90 <jkcostello90@gmail.com>
2019-06-18 01:00:47 -0400

    replace meal type during update

--- app/controllers/api/v1/weeks_controller.rb
+++ app/controllers/api/v1/weeks_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::WeeksController < ApplicationController
     end
 
     def update
+      Week.find(params[:id]).meals.where(meal_types_id: week_params[:meal_type_id]).first.try(:update_attributes, {meal_types_id: 1})
       Meal.find(week_params[:meal_id]).update!(meal_types_id: week_params[:meal_type_id])
       render status: 200, json: {}
     end
